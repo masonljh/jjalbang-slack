ccd6a328d6cc833f3750e76d8dbe83901b5e84d9
종효 이 <lee9212@smtown.com>
2019-06-11 18:04:47 +0900

    짤방 설치도 되게 변경

--- routes/index.js
+++ routes/index.js
@@ -14,8 +14,7 @@ router.get('/auth/redirect', function(req, res, next) {
     uri: 'https://slack.com/api/oauth.access?code='
         + req.query.code +
         '&client_id=' + process.env.CLIENT_ID +
-        '&client_secret=' + process.env.CLIENT_SECRET +
-        '&redirect_uri=' + process.env.REDIRECT_URI,
+        '&client_secret=' + process.env.CLIENT_SECRET,
     method: 'GET'
   }
 
