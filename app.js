a5619641bb484d8f730a47da292bed229d3e4fb1
종효 이 <lee9212@smtown.com>
2019-06-11 11:04:42 +0900

    slack auth 추가

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { createMessageAdapter } = require('@slack/interactive-messages');
 
 var indexRouter = require('./routes/index');
 
@@ -18,6 +19,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Create the adapter using the app's signing secret, read from environment variable
+const slackInteractions = createMessageAdapter(process.env.SLACK_SIGNING_SECRET);
+app.use('/slack/actions', slackInteractions.expressMiddleware());
 
 app.use('/', indexRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,31 @@ var express = require('express');
 var router = express.Router();
 var jjalSelector = require('../libs/jjalSelector');
 
-/* GET home page. */
+/* GET addToSlack page. */
 router.get('/auth', function(req, res, next) {
   res.sendFile(__dirname + '/add_to_slack.html');
+});
+
+router.get('/auth/redirect', function(req, res, next) {
+  var options = {
+    uri: 'https://slack.com/api/oauth.access?code='
+        + req.query.code +
+        '&client_id=' + process.env.CLIENT_ID +
+        '&client_secret=' + process.env.CLIENT_SECRET +
+        '&redirect_uri=' + process.env.REDIRECT_URI,
+    method: 'GET'
+  }
+
+  request(options, (error, response, body) => {
+      var JSONresponse = JSON.parse(body)
+      if (!JSONresponse.ok){
+          console.log(JSONresponse)
+          res.send("Error encountered: \n"+JSON.stringify(JSONresponse)).status(200).end()
+      }else{
+          console.log(JSONresponse)
+          res.send("Success!")
+      }
+  });
 });
 
 router.post('/', function(req, res, next) {
