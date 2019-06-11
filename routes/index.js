b87c3f0d5b88c9274ae0ca3ba0985eb1c157a1fd
종효 이 <lee9212@smtown.com>
2019-06-11 15:59:02 +0900

    빠뜨린 부분 추가

--- routes/index.js
+++ routes/index.js
@@ -160,7 +160,8 @@ router.post('/', function(req, res, next) {
     }
 
     message.attachments[0].blocks[2].image_url = 'http://' + config.hostname + result[0].list_jjal.substring(startIdx, endIdx);
-    message.attachments[1].actions[3].value += message.attachments[0].blocks[2].image_url;
+    console.log(message.attachments[1].actions[3].value);
+    message.attachments[1].actions[3].value = message.attachments[1].actions[3].value + "/" + message.attachments[0].blocks[2].image_url;
 
     console.log(message.attachments[0].blocks[2].image_url);
     sendMessageToSlackResponseURL(responseUrl, message);
