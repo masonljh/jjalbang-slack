15fa272b7f2a0271a4e53591e2634e0487c6fdf9
종효 이 <lee9212@smtown.com>
2019-06-12 11:42:29 +0900

    다시 payload 부활

--- routes/index.js
+++ routes/index.js
@@ -34,7 +34,7 @@ router.post('/slack/actions', function(req, res, next) {
   res.status(200).end();
 
   var payload = JSON.parse(req.body.payload);
-  // console.log(payload);
+  console.log(payload);
 
   let responseUrl = payload.response_url;
   if (payload.token != process.env.TOKEN) {
